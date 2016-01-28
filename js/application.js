74f5bb6b8e400927d69520203b56d4cc7f2eb23a
Aleksandar Stojadinovic <astojadinovicsd@gmail.com>
2016-01-28 12:59:33 +0100

    Triangle in navigation

--- js/application.js
+++ js/application.js
@@ -1,8 +1,12 @@
 $(document).ready(function () {
 
+	function setActiveState (selected) {
+		selected.addClass("active").parent().siblings().find("a").removeClass("active");
+	}
 	
-
-			
+	$("nav").on("click","a", function() {
+		setActiveState( $(this) );
+	});
 	
 });
 
