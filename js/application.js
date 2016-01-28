959322b6166b1379178c8fe268668587c1d5c99d
Aleksandar Stojadinovic <astojadinovicsd@gmail.com>
2016-01-28 16:07:18 +0100

    Change tabs for bigger screens

--- js/application.js
+++ js/application.js
@@ -3,10 +3,18 @@ $(document).ready(function () {
 	function setActiveState (selected) {
 		selected.addClass("active").parent().siblings().find("a").removeClass("active");
 	}
+
+	function changeNavTab (tab) {
+
+	   var hash = tab.hash;
+	   $(hash).show().siblings().hide();
+	}
 	
 	$("nav").on("click","a", function() {
 		setActiveState( $(this) );
+		changeNavTab(this);
 	});
+
 	
 });
 
