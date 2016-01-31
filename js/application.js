9217def106f06b7aa12a6bcd0afe0805f5ae23e8
Aleksandar Stojadinovic <astojadinovicsd@gmail.com>
2016-01-31 19:15:47 +0100

    Tabs - different behavior for different screens

--- js/application.js
+++ js/application.js
@@ -6,14 +6,31 @@ $(document).ready(function () {
 
 	function changeNavTab (tab) {
 
-	   var hash = tab.hash;
-	   $(hash).siblings().slideUp(800);
-	   $(hash).delay( 600 ).slideDown(800);
+		var hash = tab.data("url");
+
+		/* If browser width is bigger than for mobile screens (403px) */
+		/* Then change tabs with slideUp/slideDown */
+		if ( $(document).width() > 403 ) {
+		   	$(hash).siblings().slideUp(800);
+		   	$(hash).delay( 600 ).slideDown(800);
+
+		/* If its mobile screen, then only scroll down to respective tab with smooth animation */
+		} else {
+		   	$('html, body').animate({
+		       scrollTop: $(hash).offset().top
+		     }, 1000, function(){
+
+		       // when done, add hash to url
+		       // (default click behaviour)
+		       window.location.hash = hash;
+		     });
+		}
+
 	}
 	
 	$("nav").on("click","a.tab", function() {
 		setActiveState( $(this) );
-		changeNavTab(this);
+		changeNavTab( $(this) );
 	});
 
 	
